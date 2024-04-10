a77bf35b8628ced121e27fc86d8a4cd639dda241
hanakhin <hanakhin.nounimassotte@gmail.com>
2024-04-10 10:49:58 +0200

    first

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let ctx = canvas.getContext('2d');
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
 
-function Random(min,max){
+function random(min,max){
     return Math.floor(Math.random()*(max - min + 1))+min;
 }
 
 function randomColor(){
-    return `rgb(${Random(0,255)},${Random(0,255)},${Random(0,255)})`;
+    return `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`;
 }
 
 class Ball {
@@ -39,17 +39,17 @@ class Ball {
     }
 
     update(){
-        if((this.x+this.size)>=width){
+        if((this.x + this.size) >= width){
             this.velX = -(this.velX)
         }
-        if((this.x-this.size)<=0){
+        if((this.x - this.size)<=0){
             this.velX = Math.abs(this.velX)
         }
 
-        if((this.y+this.size)>=width){
+        if((this.y + this.size)>=height){
             this.velY = -(this.velY)
         }
-        if((this.y-this.size)<=0){
+        if((this.y - this.size)<=0){
             this.velY= Math.abs(this.velY)
         }
 
@@ -69,9 +69,49 @@ class Ball {
                     this.velY = -this.velY;
                     ball.velX = -ball.velX;
                     ball.velY = -ball.velY;
+
+                    ball.color = this.color = randomColor();
                 }
             }
         }
     }
 
 }
+
+const balls = [];
+
+while(balls.length < 40){
+    const size = random(2, 10)
+    const vitesseX = random(-10, 10)
+    const vitesseY = random(-10, 10)
+    const ball = new Ball(
+    random(size, width - size),
+    random(size, height - size),
+    vitesseX === 0 ? 1 : vitesseX,
+    vitesseY === 0 ? 1 : vitesseY,
+    randomColor(),
+    size
+    );
+    balls.push(ball)
+}
+
+
+function loop(){
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+    ctx.fillRect(
+        0,
+        0,
+        width,
+        height
+    );
+
+    for(const ball of balls ){
+        ball.draw();
+        ball.update();
+        ball.collisionDetection();
+    }
+
+    requestAnimationFrame(loop)
+}
+console.log(balls)
+loop();
